6691c5dcc285da9d240c627b24e5a6f7c2c48c58
eliottbourachot <eliott.bourachot@gmail.com>
2019-02-09 15:31:34 -0500

    Added UML Lab Model and generated classes

---
+++ Backend/src/main/java/ca/mcgill/ecse321/model/File.java
@@ -0,0 +1,29 @@
+package ca.mcgill.ecse321.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Id;
+
+@Entity
+public class File{
+   private Coop coop;
+   
+   @ManyToOne(optional=false)
+   public Coop getCoop() {
+      return this.coop;
+   }
+   
+   public void setCoop(Coop coop) {
+      this.coop = coop;
+   }
+   
+   private Integer id;
+
+public void setId(Integer value) {
+    this.id = value;
+}
+@Id
+public Integer getId() {
+    return this.id;
+}
+}
