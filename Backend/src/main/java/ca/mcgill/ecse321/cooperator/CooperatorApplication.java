27e67fb41376cafa60fcef593310511173bc439a
eliottbourachot <eliott.bourachot@gmail.com>
2019-02-10 10:53:57 -0500

    Added back imports that doing the code generation thing removes... issue to be looked into

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.cooperator;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CooperatorApplication{
