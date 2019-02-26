480fba30a62755b00fb661f2fb50f6687dbbc1b5
Emma Eagles <emma.eagles@mail.mcgill.ca>
2019-02-26 00:21:30 -0500

    undid a change i made to the generated code

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/Student.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/model/Student.java
@@ -27,7 +27,7 @@ public void setId(Integer value) {
 }
    private Integer coopsCompleted;
 
-public void setCoopsCompleted(Integer value) {
+pupublic void setCoopsCompleted(Integer value) {
       this.coopsCompleted = value;
 }
    public Integer getCoopsCompleted() {
