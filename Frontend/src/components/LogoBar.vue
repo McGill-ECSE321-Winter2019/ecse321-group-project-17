c2e60edb7d2fdc07365dcc066a6ff94ef646e366
kraglalbert <albert.kragl@mail.mcgill.ca>
2019-03-20 15:03:15 -0400

    Made navbar logo clickable, redirects to homepage

--- Frontend/src/components/LogoBar.vue
+++ Frontend/src/components/LogoBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-dark bg-dark" id="container">
-    <span class="navbar-brand mb-0 h1">Cooperator</span>
+    <a class="navbar-brand mb-0 h1" href="/">Cooperator</a>
   </nav>
 </template>
 
